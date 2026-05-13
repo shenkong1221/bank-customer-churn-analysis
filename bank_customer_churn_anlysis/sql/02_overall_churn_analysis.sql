99137248da4161a45449f9de4400e46558a05c24
Shen <shenkong1221@gmail.com>
2026-05-13 12:23:13 +0800

    Delete bank_customer_churn_anlysis directory

--- bank_customer_churn_anlysis/sql/02_overall_churn_analysis.sql
+++
@@ -1,4 +0,0 @@
-SELECT  COUNT(*) AS total_customers,
-		SUM(churn) AS churned_customers,
-		CONCAT(ROUND(SUM(churn) * 100/COUNT(*), 2), '%') AS churn_rate_pct
-FROM bank_customers;
--- bank_customer_churn_anlysis/sql/06_products_number_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	products_number,
-    SUM(churn) AS churned_customers,
-    CONCAT(
-		ROUND(AVG(churn) * 100, 2), 
-        '%') AS churn_rate
-FROM bank_customers
-GROUP BY products_number
-ORDER BY AVG(churn) DESC;
